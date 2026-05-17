90f64375a31d5f5f1c79d14d91276c228411168c
Soofsmoor <soofsmoor@gmail.com>
2026-05-17 12:41:09 +0200

    Player Triangle on screen

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-
-
-
-
-
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        pygame.time.Clock().tick(60)
+        dt = pygame.time.Clock().tick(60) / 1000
+        player.draw(screen)
+        
         pygame.display.flip()
 
 
