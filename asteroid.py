bd8993aacb0fbdd04542d2eda1e318fc44fbcd34
Soofsmoor <soofsmoor@gmail.com>
2026-05-18 18:02:42 +0200

    Asteroids now split when shot

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,10 @@
+from hmac import new
+import random
+
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +15,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        log_event("asteroid_split")
+        rotation = random.uniform(20, 50)
+        first_new_velocity = self.velocity.rotate(rotation)
+        second_new_velocity = self.velocity.rotate(-rotation)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        first_new_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        second_new_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        first_new_asteroid.velocity = first_new_velocity * 1.2
+        second_new_asteroid.velocity = second_new_velocity * 1.2
